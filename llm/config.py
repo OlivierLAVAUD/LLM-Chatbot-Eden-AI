2e17962287a0e9a29dd9e54832c4e7d5f9e3b6e2
OlivierLavaud <olivier.lavaud@gmail.com>
2024-05-26 18:33:40 +0200

    v1

--- llm/config.py
+++ llm/config.py
@@ -4,6 +4,6 @@ NLP_PORT = 8010
 
 EDENAI_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWRmZDViMjUtNGQzNy00ZTljLTkwZWYtMTczMmJmMWQ3OTkwIiwidHlwZSI6ImFwaV90b2tlbiJ9.vMM4AK9eazD-dKsWg1fS08ylqSprLMP3HIkIkvxqpsg"
 
-DOC_SOURCE = "olavaud.json"
+DOC_SOURCE = "olavaud.json" 
 CONTEXT = "Agit en tant qu'Avatar d'Olivier LAVAUD qui répond aux questions relatives a son cv ci-apres: """
 
